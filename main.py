b83b252c1590fdddd04567303a315956288e844a
JaneSmith <j.smith@jetbrains.com>
2018-08-20 20:19:19 +0100

    added new file

---
+++ main.py
@@ -0,0 +1,7 @@
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({number}), currently i={i}...")
+        await asyncio.sleep(1)
+        f *= i
+    return f
