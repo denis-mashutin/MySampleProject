d819862dd11173d33d5e889688aba0944c1517cb
Denis Mashutin <denis.mashutin@jetbrains.com>
2022-11-21 17:05:34 +0100

    added rst examples

---
+++ sample.py
@@ -0,0 +1,45 @@
+def example(x):
+    """
+    .. |language| replace:: Python
+
+    Title
+    =====
+
+    This is an example of |language| function documentation.
+
+    Title2
+    ------
+
+    You can use text formatting:
+
+    * *italics*
+    * **bold**
+    * ``monospace``
+    Insert links:
+    `PyCharm <https://www.jetbrains.com/pycharm/download/>`_
+
+    Provide code samples::
+
+        print('Hello, world!')
+
+    Including those with command prompt:
+
+    >>> print('Hi!')
+    Hi!
+
+    Tables are also supported:
+
+    =====  =====  ======
+       Inputs     Output
+    ------------  ------
+      A      B    A or B
+    =====  =====  ======
+    False  False  False
+    True   False  True
+    =====  =====  ======
+    :param x: description
+    """
+    pass
+
+
+
