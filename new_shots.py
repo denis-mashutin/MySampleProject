afc7b885a13606abcf503983ebb014af38efe8f9
Denis Mashutin <denis.mashutin@jetbrains.com>
2022-11-21 16:47:02 +0100

    Uncommented lines in new_shots

--- new_shots.py
+++ new_shots.py
@@ -1,15 +1,15 @@
-# 
-# a = 1  # type: int
-# 
-# 
-# def bar(param):
-#     # type: (object) -> object
-#     return param
-#
-#
-# def foo(param):
-#     b = bar(param)
-#     return b
+
+a = 1  # type: int
+
+
+def bar(param):
+    # type: (object) -> object
+    return param
+
+
+def foo(param):
+    b = bar(param)
+    return b
 
 class C:
     foo = None  # type: object
@@ -25,14 +25,14 @@ c = C('bar').foo
 
 
 
-# def bar2(param: int) -> None:
-#     # type: (int) -> None
-#     pass
+def bar2(param: int) -> None:
+    # type: (int) -> None
+    pass
 
 
-# def b(p1, p2):
-#     # type: (int) -> None
-#     pass
+def b(p1, p2):
+    # type: (int) -> None
+    pass
 
 
 def bar3(param: int):
