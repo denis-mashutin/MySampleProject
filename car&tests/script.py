6e8f5569b9d0a878a98169399236c10d0b8c05e2
PyCharmUser <pcuser@jetbrains.com>
2022-12-08 18:02:23 +0100

    Updated script for diff view demo

--- car&tests/script.py
+++ car&tests/script.py
@@ -1,15 +1,10 @@
 import sys
 from car import Car
 
-
+# The value of s in assigned from the first argument
 s = int(sys.argv[1])
 
 car = Car(s)
-car.brake()
+for i in range(2):
+    car.brake()
 print(f"Car's current speed is {car.speed}")
-
-
-
-
-
-
